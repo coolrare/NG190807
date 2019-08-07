3eef4113958b6ea83d09d92a74afbd04bce5e1ba
Will <doggy.huang@gmail.com>
2019-08-07 17:22:49 +0800

    完成路由連結設定

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,21 @@ import { Routes, RouterModule } from '@angular/router';
 import { Page1Component } from './page1/page1.component';
 import { Page2Component } from './page2/page2.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { ChartsComponent } from './charts/charts.component';
+import { TablesComponent } from './tables/tables.component';
 
 const routes: Routes = [
   {
     path: '',
     component: DashboardComponent
+  },
+  {
+    path: 'charts',
+    component: ChartsComponent
+  },
+  {
+    path: 'tables',
+    component: TablesComponent
   }
 ];
 
