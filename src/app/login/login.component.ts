5dc7088907d3f38d17bce1a08c4d77b1eb7fc9d5
Will <doggy.huang@gmail.com>
2019-08-15 19:57:06 +0800

    加入第一版 Reactive Forms

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
-import { NgForm } from '@angular/forms';
+import { NgForm, FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -9,16 +9,24 @@ import { NgForm } from '@angular/forms';
 })
 export class LoginComponent implements OnInit, OnDestroy {
 
+  form: FormGroup;
+
   data: any = {
     email: '',
     pwd: '',
     rememberMe: true
   };
 
-  constructor(private router: Router, private route: ActivatedRoute) { }
+  constructor(private router: Router, private route: ActivatedRoute, private fb: FormBuilder) { }
 
   ngOnInit() {
     document.body.className = 'bg-gradient-primary';
+
+    this.form = this.fb.group({
+      email: '',
+      pwd: '',
+      rememberMe: true
+    });
   }
 
   ngOnDestroy(): void {
